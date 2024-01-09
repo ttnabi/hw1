f53c86c0c7e34a73b78101806fdfa4232a9cb187
Geek_Tech <geecktech.och2@gmail.com>
2024-01-09 16:51:21 +0600

    'les2'

--- lessons/lesson2.py
+++ lessons/lesson2.py
@@ -1,1 +1,65 @@
-print('wergdf')
+# ООП Наследование полиморфизм ИНкапсуляцию
+
+
+class Hum(object):  # супер класс - родительский класс
+    # магический метод
+    def __init__(self, name):
+        self.name = name
+        # self.age=age
+
+    # метод
+    def Hi(self): ...
+
+    def __str__(self):
+        return 'HI'
+
+    def __len__(self):
+        return len(self)
+
+
+# экземпляр\обьект
+h = Hum('sdfg')
+h.name = 'beka'
+h.Hi()
+
+
+class Student(Hum):  # дочерний класс
+    # def __
+
+    def Hi(self):
+        print('Hello ', self.name)
+
+
+student = Student('beka')
+student.Hi()
+
+print(Student.mro())
+
+
+class Kid(Student):
+    def __init__(self, name, age, lastname):
+        # super().__init__(name)
+        Student.__init__(self, name)
+        self.age = age
+        self.last = lastname
+
+    def Hi(self):
+        Hum.Hi(self)
+
+
+c = Kid('Beka', 14, 'DZU')
+
+c.Hi()
+
+
+class Kid2(Kid):
+    def __init__(self, name, age, lastname, fly=False):
+        super().__init__(name, age, lastname)
+        self.fly = fly
+
+    def __str__(self):...
+
+
+kid = Kid2('beka', 11, 'wer', False)
+
+print(kid)
