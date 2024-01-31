bf72ee900d69daf81b139598d02c578332b769da
Geek_Tech <geecktech.och2@gmail.com>
2024-01-31 18:33:33 +0600

    'lesson8'

--- lessons/lesson7.py
+++ lessons/lesson7.py
@@ -18,12 +18,23 @@ with sqlite3.connect('op38.db') as con:
     # cur.execute('''INSERT INTO users VALUES ('BEKA',30,8)''')
     # cur.execute('''INSERT INTO users (name) VALUES ('name')''')
 
+# update - UPDATE nametable SET name=name WHERE ><=! in not or and
+
+    cur.execute('''UPDATE users SET view = 10,name="none" WHERE age=1 ''')
+
+# delete - DELETE FROM tablename WHERE условие
+    cur.execute('''DELETE FROM users WHERE rowid IN (1,3,5)''')
+
+
+# reed- SELECT ,FROM - nametable
     cur.execute('''SELECT rowid,* FROM users ORDER BY rowid desc ''')
     print('id name view age')
     for i in cur.fetchall():
         print(i)
 
-#reed
+
+
+
 
 
 # con=with sqlite3.connect('op38.db')
